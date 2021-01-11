dd08f911577e63019b45b6105b6e8de748d55512
Cesar <cesar.vargas@rookmotion.com>
2021-01-10 23:05:13 -0600

    Added card swipper

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,8 +7,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text("Hola Mundo"),
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text("Películas"),
+          backgroundColor: Colors.indigoAccent,
+          elevation: 0,
+          actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              _swiperTarjetas(),
+            ],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        //layout: SwiperLayout.STACK,
+        layout: SwiperLayout.TINDER,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        itemWidth: 350.0,
+        itemHeight: 500.0,
+        //puntitos como page controller
+        pagination: SwiperPagination(),
+        //barras en los lados
+        control: SwiperControl(),
       ),
     );
   }
