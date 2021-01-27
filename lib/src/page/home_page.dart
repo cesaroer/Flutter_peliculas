00fa8b61180ed2d1648653938f6561e816fbac4b
Cesar <cesar.vargas@rookmotion.com>
2021-01-26 23:55:53 -0600

    Added horizontal ViewPager to homePage with popular movies

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   final peliculasProvider = PeliculasProvider();
@@ -47,13 +48,26 @@ class HomePage extends StatelessWidget {
     return Container(
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Populares", style: Theme.of(context).textTheme.subtitle1),
+          Container(
+            padding: EdgeInsets.only(left: 20.0),
+            child:
+                Text("Populares", style: Theme.of(context).textTheme.subtitle1),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
           FutureBuilder(
             future: peliculasProvider.getPopulares(),
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-              snapshot.data?.forEach((p) => print(p.title));
-              return Container();
+              if (snapshot.hasData) {
+                return MovieHorizontal(peliculas: snapshot.data);
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
             },
           ),
         ],
