ec27f01641308ef1d0c5f23e574eb44feed93bbd
Cesar <cesar.vargas@rookmotion.com>
2021-02-04 23:54:38 -0600

    Added logic for select a listittle

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 //Clase para el buscador
 class DataSearch extends SearchDelegate {
+  String seleccion = "";
+
+  final peliculas = [
+    "Spiderman",
+    "Capitan america",
+    "Spiderman 2",
+    "Capitan america 2",
+    "Shazam",
+    "Batman",
+    "Superman"
+  ];
+  final peliculasRecientes = [
+    "Spiderman",
+    "Capitan america",
+  ];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     //Las acciones de nuestro AppBar , para cancelar o limpiar la busqueda
@@ -35,12 +51,40 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados a mostrar
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Text(seleccion),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Sugerencias que aparecen al teclear
-    return Container();
+    // Mostramos un list o table view con elementos
+
+    //Si query es "" entonces dejo peliculas sugeridas,
+    //si no filtro sobre la lista de peliculas y lo muestro
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((pelicula) => pelicula.toLowerCase().startsWith(query))
+            .toList();
+
+    return ListView.builder(
+        itemCount: listaSugerida.length,
+        itemBuilder: (context, i) {
+          return ListTile(
+            leading: Icon(Icons.movie),
+            title: Text(listaSugerida[i]),
+            onTap: () {
+              seleccion = listaSugerida[i];
+              showResults(context);
+            },
+          );
+        });
   }
 }
